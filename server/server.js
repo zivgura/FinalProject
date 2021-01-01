b833762f86fb42cbf610ac72ded759333a84af04
Ziv <zivgura6@gmail.com>
2021-01-01 17:17:17 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+const cors = require('cors')
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+}
+
+app.use(cors(corsOptions))
+app.get('/api/greeting', (req, res) => {
+    const name = req.query.name || 'World';
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.listen(3001, () =>
+    console.log('Express server is running on localhost:3001')
+);
+
---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            greeting: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({name: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        fetch(`http://localhost:3001/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+            .then(response => response.json())
+            .then(state => this.setState(state));
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor="name">Enter your name: </label>
+                        <input
+                            id="name"
+                            type="text"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                    <p>{this.state.greeting}</p>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default App;
